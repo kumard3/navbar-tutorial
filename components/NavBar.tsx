3876ff3ef085d4175a073d9daec79f7357a326c7
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-16 23:03:11 +0530

    nav bar added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,20 +1,31 @@
 export default function NavBar() {
-    return (
-        <div className="header header-fixed">
-        <div className="navbar container">
-            <div className="logo"><a href="#home">LOGO</a></div>
-            <input type="checkbox" id="navbar-toggle" />
-            <label htmlFor="navbar-toggle"><i></i></label>
-            <nav className="menu">
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="#contacts">Contacts</a></li>
-                </ul>
-            </nav>        
+  return (
+    <div className="header header-fixed">
+      <div className="navbar container">
+        <div className="logo">
+          <a href="#home">LOGO</a>
         </div>
+        <input type="checkbox" id="navbar-toggle" />
+        <label htmlFor="navbar-toggle">
+          <i></i>
+        </label>
+        <nav className="menu">
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="#contacts">Contacts</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
-          
-    )
+  );
 }
